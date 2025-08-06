16922ceeeaecd7035d22f8f1940bf8ab149c33fe
hopoduck <gksqudwh1@naver.com>
2025-08-06 17:21:31 +0900

    2025-08-06 추가

---
+++ src/11651.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+fs.readFileSync = () => `5
+0 4
+1 2
+1 -1
+2 2
+3 3`;
+
+let [_, ...list] = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+const result = list
+  .map((xy) => xy.split(" ").map(Number))
+  .sort((a, b) => {
+    if (a[1] !== b[1]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  })
+  .map((v) => v.join(" "))
+  .join("\n");
+
+console.log(result);
